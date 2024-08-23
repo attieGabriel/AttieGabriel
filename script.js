0164d6a7ccd57ac617217d57a7483caeabfcca3e
Gabriel Gomes Attie <gabrielgattie@gmail.com>
2024-08-23 14:37:52 -0300

    aaaa

---
+++ script.js
@@ -0,0 +1,68 @@
+function mudarConteudo() {
+    if( document.getElementById("translate").innerHTML == "Translate to english"){
+      document.getElementById("translate").innerHTML = "Traduza para portugues";
+      document.getElementById("welcome").innerHTML = "Welcome to my website";
+      document.getElementById("about_me").innerHTML = "About me";
+      document.getElementById("expe_me").innerHTML = "Experiences";
+      document.getElementById("portifolio_me").innerHTML = "Projects";
+      document.getElementById("languages_me").innerHTML = "Languages";
+      document.getElementById("contact_me").innerHTML = "Contact";
+      document.getElementById("about").innerHTML = "About me";
+      document.getElementById("about_text").innerHTML = "Computer science student with experience in research (simulation and analysis of models for route planning in smart cities) for 1 and a half years, IT assistant for 6 months and mendix developer since 2023. I have experience with: Java | Python | C | VM | Sumo | Veins | Omnet++ | SQL | Git | AWs | Mendix";
+      document.getElementById("expe").innerHTML = "Experiences";
+      document.getElementById("initiation").innerHTML = "Scientific initiation";
+      document.getElementById("initiation_1").innerHTML = "Research and simulation of vehicular networks with 5G";
+      document.getElementById("initiation_2").innerHTML = "1 year and a half of research";
+      document.getElementById("support").innerHTML = "Technical support";
+      document.getElementById("support_1").innerHTML = "Internship at SEJUC providing technical support and computer repair";
+      document.getElementById("support_2").innerHTML = "Network monitoring";
+      document.getElementById("support_3").innerHTML = "6 months internship";
+      document.getElementById("intern").innerHTML = "Mendix Internship";
+      document.getElementById("intern_1").innerHTML = "Internship as mendix developer at USERH";
+      document.getElementById("intern_2").innerHTML = "Low-code development of multiple systems";
+      document.getElementById("intern_3").innerHTML = "Since August/2023";
+      document.getElementById("portfolio").innerHTML = "Projects";
+      document.getElementById("kitchen").innerHTML = "Cozinha Virtual: Social network for culinary lovers";
+      document.getElementById("2d").innerHTML = "2D Game: Jumping and avoiding obstacles game";
+      document.getElementById("language").innerHTML = "Languages";
+      document.getElementById("pt").innerHTML = "Fluent Portuguese";
+      document.getElementById("en").innerHTML = "Fluent English";
+      document.getElementById("es").innerHTML = "Basic Spanish";
+      document.getElementById("fr").innerHTML = "Basic French";
+      document.getElementById("contact").innerHTML = "Contact";
+      document.getElementById("phone").innerHTML = "Telephone: : +55 (79) 98154-6130 <br >E-mail: gabrielgattie@gmail.com <br> <a href=https://github.com/attieGabriel target=_blank>Github: attieGabriel</a>";
+    }
+    else {
+      document.getElementById("translate").innerHTML = "Translate to english";
+      document.getElementById("welcome").innerHTML = "Bem-vindo ao Meu Site";
+      document.getElementById("about_me").innerHTML = "Sobre mim";
+      document.getElementById("expe_me").innerHTML = "Experiência";
+      document.getElementById("portifolio_me").innerHTML = "Projetos";
+      document.getElementById("languages_me").innerHTML = "Idiomas";
+      document.getElementById("contact_me").innerHTML = "Contato";
+      document.getElementById("about").innerHTML = "Sobre Mim";
+      document.getElementById("about_text").innerHTML = "Estudante de ciências da computação com experiência em pesquisa(simulação e análise de modelos para o planejamento de rotas em cidades inteligentes) por 1 ano e meio e auxiliar de TI por 6 meses. Tenho experiência com: Java | Python | C | VM | Sumo | Veins | Omnet++ | SQL | Git | AWs | Mendix";
+      document.getElementById("expe").innerHTML = "Experiência";
+      document.getElementById("initiation").innerHTML = "Iniciação Científica";
+      document.getElementById("initiation_1").innerHTML = "Pesquisa e simulação de redes veiculares com 5G";
+      document.getElementById("initiation_2").innerHTML = "1 ano e meio de pesquisa";
+      document.getElementById("support").innerHTML = "Suporte Técnico";
+      document.getElementById("support_1").innerHTML = "Estágio na SEJUC fornecendo suporte técnico e conserto de computadores";
+      document.getElementById("support_2").innerHTML = "Monitoramento de redes";
+      document.getElementById("support_3").innerHTML = "6 meses de estágio";
+      document.getElementById("intern").innerHTML = "Estágio Mendix";
+      document.getElementById("intern_1").innerHTML = "Estágio como desenvolvedor mendix na USERH";
+      document.getElementById("intern_2").innerHTML = "Desenvolvimento em low-code de sistemas diversos";
+      document.getElementById("intern_3").innerHTML = "Desde agosto/2023";
+      document.getElementById("portfolio").innerHTML = "Projetos";
+      document.getElementById("kitchen").innerHTML = "Cozinha Virtual: Rede social para apaixonados pela culinária";
+      document.getElementById("2d").innerHTML = "Jogo 2D: Jogo de pular e desviar de obstáculos";
+      document.getElementById("language").innerHTML = "Idiomas";
+      document.getElementById("pt").innerHTML = "Português fluente";
+      document.getElementById("en").innerHTML = "Inglês fluente";
+      document.getElementById("es").innerHTML = "Espanhol básico";
+      document.getElementById("fr").innerHTML = "Francês básico";
+      document.getElementById("contact").innerHTML = "Contato";
+      document.getElementById("phone").innerHTML = "Telefone: (79) 98154-6130 <br>Email: gabrielgattie@gmail.com <br> <a href=https://github.com/attieGabriel target=_blank>Github: attieGabriel</a>";
+    }
+    }
